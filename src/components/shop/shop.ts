e89c320dab559cdf77bfff454411a2d4aef06c86
Shrouk Salah <shrouksalah707@outlook.com>
2026-07-13 20:36:50 +0300

    product card

--- src/components/shop/shop.ts
+++ src/components/shop/shop.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { ProductCard } from "../product-card/product-card";
+import { Product } from '../../app/product';
 
 @Component({
   selector: 'app-shop',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './shop.html',
   styleUrl: './shop.css',
 })
-export class Shop {}
+export class Shop {
+
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'Jaipur Art and Rugs Decorative, Cushion',
+      price: 149.99,
+      image: 'images/pro1.jpg',
+    },
+     {
+      id: 2,
+      name: 'ASTRID 20 Cms Blue Cotton Travel Pouch',
+      price: 255,
+      image: 'images/pro2.jpg',
+    },
+     {
+      id: 3,
+      name: 'Teng Jin Nantucket Round Handicraft',
+      price: 149.99,
+      image: 'images/pro3.jpg',
+    }
+  ];
+
+}
